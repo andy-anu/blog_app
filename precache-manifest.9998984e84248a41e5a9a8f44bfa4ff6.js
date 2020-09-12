be32b77bd931368f7d70070fdb92bd55e60c655f
andy-anu <8765anurag@gmail.com>
2020-09-12 16:51:30 +0530

    Updates

---
+++ precache-manifest.9998984e84248a41e5a9a8f44bfa4ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41d7233568081e1affc477c41928bc5",
+    "url": "/chat_app/index.html"
+  },
+  {
+    "revision": "9a71730911df60bfbba8",
+    "url": "/chat_app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9a71730911df60bfbba8",
+    "url": "/chat_app/static/js/2.522124ab.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/chat_app/static/js/2.522124ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4adc992e8b93bbff57d",
+    "url": "/chat_app/static/js/main.ae6743eb.chunk.js"
+  },
+  {
+    "revision": "9a57248feacbfa2d4c7b",
+    "url": "/chat_app/static/js/runtime-main.b08097ed.js"
+  }
+]);
